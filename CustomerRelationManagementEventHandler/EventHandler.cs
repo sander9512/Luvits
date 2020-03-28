c72875d8185bbe3f643b48102dfa3b1f261d728b
sander9512 <s.c.schep@gmail.com>
2020-03-28 20:08:23 +0100

    Added CustomerRelationManagementEventHandler

---
+++ CustomerRelationManagementEventHandler/EventHandler.cs
@@ -0,0 +1,45 @@
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json.Linq;
+using Pitstop.Infrastructure.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Serilog;
+
+namespace Luvits.CustomerRelationManagementEventHandler
+{
+    public class EventHandler : IHostedService, IMessageHandlerCallback
+    {
+        IMessageHandler _messageHandler;
+
+        public EventHandler(IMessageHandler messageHandler) {
+            _messageHandler = messageHandler;
+        }
+        
+        public void Start() {
+            _messageHandler.Start(this);
+        }
+
+        public void Stop() {
+            _messageHandler.Stop();
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken) {
+            _messageHandler.Start(this);
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken) {
+            _messageHandler.Stop();
+            return Task.CompletedTask;
+        }
+
+        public async Task<bool> HandleMessageAsync(string messageType, string message) {
+            Log.Information("Event with messagetype " + messageType + " logged");
+            Log.Information("Event with message " + message + " logged");
+
+            return true;
+        }
+    }
+}
